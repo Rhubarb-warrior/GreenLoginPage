cf08dc673f28fddbb90eed7d89ebc68762827854
Rhubarb-warrior <65822593+Rhubarb-warrior@users.noreply.github.com>
2020-09-16 12:50:02 +0100

    Add files via upload

---
+++ build/precache-manifest.76def22c609db136fb7bf8b034a17047.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c1d393912e189a6ade1a0d1adb3d31e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a0b16c5ee4f4bf82612",
+    "url": "/static/css/main.7f90e3a3.chunk.css"
+  },
+  {
+    "revision": "bab9311a5f344513cfed",
+    "url": "/static/js/2.224513ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.224513ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a0b16c5ee4f4bf82612",
+    "url": "/static/js/main.ee970246.chunk.js"
+  },
+  {
+    "revision": "062f24ecfed40e6bdac7",
+    "url": "/static/js/runtime-main.bf8463eb.js"
+  },
+  {
+    "revision": "9da97edd306f798b1cb2eb6dca334e22",
+    "url": "/static/media/logo-r.9da97edd.svg"
+  }
+]);
